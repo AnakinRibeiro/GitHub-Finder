458e7208267326c87bd7eec0dd82bb4b8e1a7ae3
Anakin Ribeiro <eduardor.contato@outlook.com>
2019-06-19 01:55:25 -0300

    19/06

--- src/App.js
+++ src/App.js
@@ -2,15 +2,47 @@ import React, { Component } from 'react';
 
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
+import api from './services/Api';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    // Github API Key
+    const response = await api.get(`users?client_id=
+    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+    ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ users: response.data, loading: false });
+  }
+
+  // Search Github users
+  searchUsers = async text => {
+    this.setState({ loading: true });
+
+      const response = await api.get(`/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  );
+
+    this.setState({ users: response.data.items, loading: false });
+  };
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className="container">
-          <Users />
+          <Search searchUsers={this.searchUsers} />
+          <Users loading={this.state.loading}  users={this.state.users} />
         </div>
       </div>
     )
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-import api from '../../services/Api';
 
-class Users extends Component {
-    state = {
-        users: [],
-    };
+const Users = ({ users, loading }) => {
+        if (loading) {
+            return <Spinner />
+        } else {
+            return (
+                <div style={userStyle}>
+                    {users.map(user => (
+                        <UserItem key={user.id} user={user} />
+                    ))}
+                </div>
+            )
+        }     
+}
 
-    async componentDidMount() {
-        const response = await api.get('users');
-
-        this.setState({ users: response.data });
-    }
-
-    render() {
-        return (
-            <div style={userStyle}>
-                {this.state.users.map(user => (
-                    <UserItem key={user.id} user={user} />
-                ))}
-            </div>
-        )
-    }
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
 }
 
 const userStyle = {
