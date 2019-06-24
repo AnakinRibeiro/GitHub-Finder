14b8a74add225cba82dc8bb145d430fa5a3d6d2d
Anakin Ribeiro <eduardor.contato@outlook.com>
2019-06-24 11:13:37 -0300

    Commit Final

--- src/App.js
+++ src/App.js
@@ -1,65 +1,38 @@
-import React, { Fragment, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import api from './services/Api';
 
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 
 import './App.css';
 
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
 const App = () => {
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [alert, setAlert] = useState(null);
-
-  // Get Users repos
-  const getUserRepos = async (username) => {
-    setLoading(true);
-
-    const response = await api.get(`/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
-      process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setRepos(response.data);
-    setLoading(false);
-  }
-
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-    setTimeout(() => setAlert(null), 5000);
-  }
 
   return (
     <GithubState>
-      <Router>
-        <div className='App'>
-          <Navbar />
-          <div className="container">
-            <Alert alert={alert} />
-            <Switch>
-              <Route exact path='/' render={props => (
-                <Fragment>
-                  <Search setAlert={showAlert} />
-                  <Users />
-                </Fragment>
-              )} />
-              <Route exact path='/about' component={About} />
-              <Route exact path='/user/:login' render={props => (
-                <User {...props}
-                  getUserRepos={getUserRepos}
-                  repos={repos} />
-              )} />
-            </Switch>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   )
 }
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -1,5 +1,4 @@
 import React, { useReducer } from 'react';
-import axios from 'axios';
 import GithubContext from './githubContext';
 import GithubReducer from './githubReducer';
 import api from '../../services/Api';
@@ -10,6 +9,17 @@ import {
     GET_USER,
     GET_REPOS
 } from '../types';
+
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== 'production') {
+    githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+    githubClientId = process.env.GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
 
 const GithubState = props => {
     const initialState = {
@@ -25,9 +35,8 @@ const GithubState = props => {
     const searchUsers = async text => {
         setLoading();
 
-        const response = await api.get(`/search/users?q=${text}&client_id=${
-            process.env.REACT_APP_GITHUB_CLIENT_ID
-            }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        const response = await api.get(`/search/users?q=${text}&client_id=${githubClientId}&
+        client_secret=${githubClientSecret}`
         );
 
         dispatch({
@@ -40,9 +49,8 @@ const GithubState = props => {
     const getUser = async (username) => {
         setLoading();
 
-        const response = await api.get(`/users/${username}?client_id=${
-            process.env.REACT_APP_GITHUB_CLIENT_ID
-            }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        const response = await api.get(`/users/${username}?client_id=${githubClientId}&
+        client_secret=${githubClientSecret}`
         );
 
         dispatch({
@@ -52,6 +60,18 @@ const GithubState = props => {
     }
 
     // Get Repos
+    const getUserRepos = async (username) => {
+        setLoading();
+
+        const response = await api.get(`/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&
+        client_secret=${githubClientSecret}`
+        );
+
+        dispatch({
+            type: GET_REPOS,
+            payload: response.data
+        })
+    }
 
     // Clear Users
     const clearUsers = () => dispatch({ type: CLEAR_USERS })
@@ -67,7 +87,8 @@ const GithubState = props => {
             loading: state.loading,
             searchUsers,
             clearUsers,
-            getUser
+            getUser,
+            getUserRepos
         }}
     >
         {props.children}
